636d21269fafd3ee55e08e12f03c05abfbe98877
Josh Thurston <Joshthurston92@gmail.com>
2022-02-08 17:16:26 -0700

    basics

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log('testing testing 1 2 3 4');
+function add(num1, num2) {
+    return num1 + num2;
+}
+const number1 = 1;
+const number2 = 2.8;
+let result = add(number1, number2);
+console.log(result);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+console.log('testing testing 1 2 3 4');
+
+function add(num1: number, num2: number) {
+  return num1 + num2
+}
+
+const number1 = 1;
+const number2 = 2.8;
+
+let result = add(number1, number2);
+console.log(result)
